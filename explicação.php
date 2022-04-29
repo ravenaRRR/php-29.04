20a9b8b0a3093d3d6176a872ed0e0ea1e855d4a5
ravenaRRR <raphaelamirandola@hotmail.com>
2022-04-29 08:50:34 -0300

    save2

---
+++ explicação.php
@@ -0,0 +1,26 @@
+<?php
+/*
+
+Estrutura condicional - Switch case 
+
+-> Switch recebe um valor como parâmetro e verifica se ele atende algumas das condições espeecificadas.
+Em caso positivo, o trecho de código relacionado a essa condição é executado.
+-> Diferente da estrutura if/else, o switch/case avalia apenas condições de igualdade. 
+Ou seja, ele verifica se o valor passado como parâmetro é igual a alguma das condições especificadas em seu corpo.
+Esse comportamento equivale a ultilização de vários if/elses em sequência, porém com uma sintaxe mais enxulta e de fácil leitura.
+
+
+*/
+
+Switch (expressão)
+case 1
+//código a ser executado
+break;
+
+case 2
+//código a ser executado
+break;
+
+default
+//código a ser executado
+break;
