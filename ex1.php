b8309f1afd7f6d1b87ceb15cea11b9b45b7814d2
ravenaRRR <raphaelamirandola@hotmail.com>
2022-04-29 08:57:33 -0300

    save3

--- ex1.php
+++ ex1.php
@@ -4,11 +4,17 @@ switch($sorteio){
     case 1: 
         echo "Número um, você tirou Marcelo, ladrão, meio fraco mas fofo e engraçado";
         break;
+        case 2:
         echo "Número dois, você tirou Nat, fofa e simpática";
         break;
+        case 3: 
         echo "Número três, você tirou Maju, durona mas uma amor";
         break; 
+        case 4:
+        echo "Número quatro, você tirou Raphaela, a única normal do grupo, legal e pata do Marcelo";
+        break; 
+
         default: //comparar ao else estrutura if
-        echo "Número quatro, você tirou Luiza, um amor e delicada"; 
+        echo "Número zero, você tirou Luiza, um amor e delicada"; 
         break;
 }
